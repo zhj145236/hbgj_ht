b28d704d9d1716bd08b293ae8819fe9200563215
堂吉诃德 <1933544489@qq.com>
2020-03-20 12:08:15 +0800

    文件上传完善好了

--- src/main/java/com/yusheng/hbgj/controller/FileController.java
+++ src/main/java/com/yusheng/hbgj/controller/FileController.java
@@ -29,71 +29,74 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/files")
 public class FileController {
 
-	@Autowired
-	private FileService fileService;
-	@Autowired
-	private FileInfoDao fileInfoDao;
+    @Autowired
+    private FileService fileService;
+    @Autowired
+    private FileInfoDao fileInfoDao;
 
-	@LogAnnotation
-	@PostMapping
-	@ApiOperation(value = "文件上传")
-	public FileInfo uploadFile(MultipartFile file) throws IOException {
-		return fileService.save(file);
-	}
-
+    @LogAnnotation
+    @PostMapping
+    @ApiOperation(value = "文件上传")
+    public FileInfo uploadFile(MultipartFile file) throws IOException {
+        return fileService.save(file);
+    }
 
 
     /**
-	 * layui富文本文件自定义上传
-	 * 
-	 * @param file
-	 * @param domain
-	 * @return
-	 * @throws IOException
-	 */
-	@LogAnnotation
-	@PostMapping("/layui")
-	@ApiOperation(value = "layui富文本文件自定义上传")
-	public LayuiFile uploadLayuiFile(MultipartFile file, String domain) throws IOException {
-		FileInfo fileInfo = fileService.save(file);
+     * layui富文本文件自定义上传
+     *
+     * @param file
+     * @param domain
+     * @return
+     * @throws IOException
+     */
+    @LogAnnotation
+    @PostMapping("/layui")
+    @ApiOperation(value = "layui富文本文件自定义上传")
+    public LayuiFile uploadLayuiFile(MultipartFile file, String domain) throws IOException {
+        FileInfo fileInfo = fileService.save(file);
 
-		LayuiFile layuiFile = new LayuiFile();
-		layuiFile.setCode(0);
-		LayuiFile.LayuiFileData data = new LayuiFile.LayuiFileData();
-		layuiFile.setData(data);
-		data.setSrc(domain + "/files" + fileInfo.getUrl());
-		data.setTitle(file.getOriginalFilename());
+        LayuiFile layuiFile = new LayuiFile();
+        layuiFile.setCode(0);
+        LayuiFile.LayuiFileData data = new LayuiFile.LayuiFileData();
+        layuiFile.setData(data);
+        data.setSrc(domain + "/files" + fileInfo.getUrl());
+        data.setTitle(file.getOriginalFilename());
 
-		return layuiFile;
-	}
+        return layuiFile;
+    }
 
-	@GetMapping
-	@ApiOperation(value = "文件查询")
-	@RequiresPermissions("sys:file:query")
-	public PageTableResponse listFiles(PageTableRequest request) {
-		return new PageTableHandler(new PageTableHandler.CountHandler() {
+    @GetMapping
+    @ApiOperation(value = "文件查询")
+    @RequiresPermissions("sys:file:query")
+    public PageTableResponse listFiles(PageTableRequest request) {
+        return new PageTableHandler(new PageTableHandler.CountHandler() {
 
-			@Override
-			public int count(PageTableRequest request) {
-				return fileInfoDao.count(request.getParams());
-			}
-		}, new PageTableHandler.ListHandler() {
+            @Override
+            public int count(PageTableRequest request) {
+                return fileInfoDao.count(request.getParams());
+            }
+        }, new PageTableHandler.ListHandler() {
 
-			@Override
-			public List<FileInfo> list(PageTableRequest request) {
-			     request.getParams().put("orderBy","  ORDER BY createTime DESC  ");
-				List<FileInfo> list = fileInfoDao.list(request.getParams(), request.getOffset(), request.getLimit());
-				return list;
-			}
-		}).handle(request);
-	}
+            @Override
+            public List<FileInfo> list(PageTableRequest request) {
 
-	@LogAnnotation
-	@DeleteMapping("/{id}")
-	@ApiOperation(value = "文件删除")
-	@RequiresPermissions("sys:file:del")
-	public void delete(@PathVariable String id) {
-		fileService.delete(id);
-	}
+
+                request.getParams().putIfAbsent("orderBy", "  ORDER BY createTime DESC  ");
+
+
+                List<FileInfo> list = fileInfoDao.list(request.getParams(), request.getOffset(), request.getLimit());
+                return list;
+            }
+        }).handle(request);
+    }
+
+    @LogAnnotation
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "文件删除")
+    @RequiresPermissions("sys:file:del")
+    public void delete(@PathVariable String id) {
+        fileService.delete(id);
+    }
 
 }
