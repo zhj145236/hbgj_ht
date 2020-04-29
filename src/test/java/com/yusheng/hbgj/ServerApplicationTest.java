05d71facd8bbe9c216581a720612c5f45016e24f
堂吉诃德 <1933544489@qq.com>
2020-04-29 15:53:07 +0800

    配置信息加密

--- src/test/java/com/yusheng/hbgj/ServerApplicationTest.java
+++ src/test/java/com/yusheng/hbgj/ServerApplicationTest.java
@@ -1,9 +1,7 @@
 package com.yusheng.hbgj;
 
-import com.yusheng.hbgj.utils.MysqlDbService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,17 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ServerApplicationTest {
 
 
-    @Autowired
-    private MysqlDbService mysqlDbService;
-
     @Test
     public void test0() {
 
 
-       // mysqlDbService.dbBackUp();
-        //System.out.println("备份成功！");
-
-        //mysqlDbService. ();
 
 
     }
--- src/test/java/com/yusheng/hbgj/any/T1.java
+++ src/test/java/com/yusheng/hbgj/any/T1.java
@@ -1,6 +1,10 @@
 package com.yusheng.hbgj.any;
 
+import com.ulisesbocchio.jasyptspringboot.encryptor.DefaultLazyEncryptor;
 import com.yusheng.hbgj.utils.MathBaseUtil;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.core.env.StandardEnvironment;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -17,43 +21,27 @@ public class T1 {
 
     public static void main(String[] args) {
 
-        //过滤得到sql 文件大小》10KB
+
+        // 加密用的密码（盐值）见 有道云笔记中
+        System.setProperty("jasypt.encryptor.password", "");
+        StringEncryptor stringEncryptor = new DefaultLazyEncryptor(new StandardEnvironment());
 
 
-        File f = new File("D:\\download\\");
-        if (f.isDirectory()) {
+        String mysqlOriginPswd = "json@1109";
+        String redisOriginPswd = "hbgj@Redis#2020$";
 
-            File[] files = f.listFiles(new FileFilter() {
-                @Override
-                public boolean accept(File fi) {
-                    return fi.getName().endsWith(".sql") && !fi.isDirectory() && (fi.length() > 20 * 1024);
-                }
-            });
-
-            Arrays.sort(files, new Comparator<File>() {
-                public int compare(File f1, File f2) {
-                    long diff = f1.lastModified() - f2.lastModified();
-                    if (diff > 0)
-                        return -1;
-                    else if (diff == 0)
-                        return 0;
-                    else
-                        return 1;//如果 if 中修改为 返回-1 同时此处修改为返回 1  排序就会是递减
-                }
-
-                public boolean equals(Object obj) {
-                    return true;
-                }
-
-            });
-
-            for (int i = 0; i < files.length; i++) {
-
-                System.out.println(files[i].getName() + "<<<<<<,," + files[i].length());
-            }
+        //要加密的数据（数据库的用户名或密码）
+        String A = stringEncryptor.encrypt(mysqlOriginPswd);
+        String B = stringEncryptor.encrypt(redisOriginPswd);
+        // 注意：每次生成的密码是不一样的, 但是通过密钥,可以解密成一样的明文.
+        System.out.println("Mysql加密:" + A);
+        System.out.println("Redis加密:" + B);
 
 
-        }
+        String PASSA = stringEncryptor.decrypt("/ST/BTZ+BtLm/kdWVBMXlfL5rPVl5NgYXwJL3PSqZ7AIei4IEEGbkoFzejPW01I9");
+        String PASSB = stringEncryptor.decrypt("XaemAvJovXdExUlVkxC7bcKgpPJQXlW1EH3N+Q24Kgr5i0LjpNRbHJlBa5vF6FvmIn5dc9tLfANrHdhuy4HdMg==");
+
+        System.out.println("密码还原：" + PASSA + "\n" + PASSB);
 
 
     }
